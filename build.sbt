bc79c881d016da7291751566193deaf25af45d43
ismet ozozturk <iozozturk@gmail.com>
2019-03-19 15:43:12 +0100

    Simple pipeline introduction with kafka consumer and elastic indexing

--- build.sbt
+++ build.sbt
@@ -5,5 +5,10 @@ version := "0.1"
 scalaVersion := "2.12.8"
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-http" % "10.1.7"
+  "com.typesafe.akka" %% "akka-http" % "10.1.7",
+  "com.typesafe.akka" %% "akka-stream-kafka" % "1.0.1",
+  "com.typesafe.akka" %% "akka-slf4j" % "2.5.21",
+  "ch.qos.logback" % "logback-classic" % "1.2.3",
+  "org.elasticsearch.client" % "transport" % "6.6.2",
+  "com.typesafe.play" %% "play-json" % "2.7.2"
 )
--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -1,4 +1,21 @@
+import java.net.InetAddress
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
+import org.elasticsearch.common.settings.Settings
+import org.elasticsearch.common.transport.TransportAddress
+import org.elasticsearch.transport.client.PreBuiltTransportClient
+
 
 object Runner extends App {
-  println("hello world")
+  implicit val actorSystem: ActorSystem = ActorSystem()
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  val config = ConfigFactory.load
+  private val esHost = config.getString("elasticsearch.host")
+
+  val esClient = new PreBuiltTransportClient(Settings.EMPTY)
+    .addTransportAddress(new TransportAddress(InetAddress.getByName(esHost), 9300))
+
+  new Pipeline(config, esClient).init()
 }
